b5157a6ec62e292680e7526e336af0ee50dea047
smalltimebenny <ben.lindquist@gmail.com>
2022-08-24 16:14:30 -0400

    hop eit's ok

---
+++ test.js
@@ -0,0 +1,13 @@
+// function myFunction(myNum) {
+//      let x = myNum;
+//      let y = 33;
+//      // return myNum;
+//      console.log(x + y);
+// }
+
+// myFunction(3);
+let x = 10
+
+if (x > 5 && x < 20){
+     console.log("worked")
+}
